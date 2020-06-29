57350706f08f305ac28468e3cbac68482d9f9e56
jbarnobal <behind.red.ice@gmail.com>
2020-06-29 12:27:43 +0800

    config routes for api versioning

--- app/controllers/projects_controller.rb
+++
@@ -1,6 +0,0 @@
-class ProjectsController < ApplicationController
-	def index
-    @projects = Project.all
-    render json: @projects
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects
+  concern :api_base do
+    resources :projects
+  end   
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
