11b1d4163ce3421140b45dcb5bab20040c24741a
jbarnobal <behind.red.ice@gmail.com>
2021-07-11 19:39:44 +0800

    add crud but not complete

--- app/controllers/v1/projects_controller.rb
+++ app/controllers/v1/projects_controller.rb
@@ -3,4 +3,20 @@ class V1::ProjectsController < ApplicationController
     projects = Project.all
     render json: projects
 	end
+    def show
+        project  = Project.find(params[:id])
+        render json: project
+    end
+
+    def create
+        # to do ..
+    end
+
+    def update
+        # to do ..
+    end
+
+    def delete
+        # to do ..
+    end
 end
